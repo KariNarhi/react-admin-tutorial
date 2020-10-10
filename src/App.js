704fdc0a2784a159f3c4a28e9bb8a0bf2b85d0bc
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-10-10 21:28:36 +0300

    Add login page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { Admin, Resource } from "react-admin";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import Dashboard from "./Dashboard";
+import authProvider from "./authProvider";
 import jsonServerProvider from "ra-data-json-server";
 import PostIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin
+    dashboard={Dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
     <Resource
       name="posts"
       list={PostList}
